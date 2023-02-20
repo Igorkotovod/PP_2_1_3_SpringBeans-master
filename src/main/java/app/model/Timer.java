713f52588a30d746d473f8a1719f2aa7704fe259
Igor Pavlov <my-mail.msk@mail.ru>
2023-02-20 11:04:27 +0300

    Initial commit

---
+++ src/main/java/app/model/Timer.java
@@ -0,0 +1,16 @@
+package app.model;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Timer {
+    @Autowired
+    private Timer timer;
+    private Long nanoTime = System.nanoTime();
+
+    public Long getTime() {
+        return nanoTime;
+    }
+}
